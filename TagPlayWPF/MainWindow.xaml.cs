c47f888b5967c3f5d9232515ef663dfe0f75f166
Q <k_yuuu@mail.ru>
2022-08-18 20:03:00 +0300

    Add project files.

---
+++ TagPlayWPF/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TagPlayWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            field = new TagPlay.RandomField();
+            InitButtons();
+            ActivateNeedButton();
+        }
+
+        //Fields
+        private const int rang = 4;
+        private MyButton[,] buttons;
+        private TagPlay.RandomField field;
+
+
+        //Functions
+        private void InitButtons()
+        {
+            buttons = new MyButton[rang, rang];
+
+            for (int i = 0; i < rang; i++)
+                for (int j = 0; j < rang; j++)
+                {
+                    buttons[i, j] = new MyButton();
+                    buttons[i, j].I = i;
+                    buttons[i, j].J = j;
+                    buttons[i, j].Content = field.getNumber(i,j).ToString();
+                    buttons[i, j].Style = (Style)Resources["btnTag"];
+                    buttons[i, j].IsEnabled = false;
+                    Grid.SetColumn(buttons[i, j], j);
+                    Grid.SetRow(buttons[i, j], i);
+                    MainGrid.Children.Add(buttons[i, j]);
+                }
+            buttons[field.indexI0, field.indexJ0].Content = "";
+        }
+
+        private void ActivateNeedButton()
+        {
+            int i0 = field.indexI0;
+            int j0 = field.indexJ0;
+            if (i0 + 1 < 4)             
+                buttons[i0 + 1, j0].IsEnabled = true;
+            if(i0 -1 > -1)
+                buttons[i0 - 1, j0].IsEnabled = true;
+            if (j0 + 1 < 4)
+                buttons[i0, j0 + 1].IsEnabled = true;
+            if (j0 - 1 > -1)
+                buttons[i0, j0 - 1].IsEnabled = true; 
+        }
+
+        private void DisableAllButton()
+        {
+            for (int i = 0; i < rang; i++)
+                for (int j = 0; j < rang; j++)
+                    buttons[i, j].IsEnabled = false;
+        }
+
+        private void DisplayField()
+        {
+            for (int i = 0; i < rang; i++)
+                for (int j = 0; j < rang; j++)
+                {
+                    buttons[i, j].Content = field.getNumber(i, j).ToString();
+                }
+            buttons[field.indexI0, field.indexJ0].Content = "";
+        }
+
+        //Handlers
+        private void btnTag_Click(object sender, RoutedEventArgs e)
+        {
+            var button = e.Source as MyButton;
+
+            field.swapNumberByI(button.I);
+            field.swapNumberByJ(button.J);
+            DisableAllButton();
+            ActivateNeedButton();
+            DisplayField();
+            if(field.Check())
+            {
+                DisableAllButton();
+            }
+            e.Handled = true;
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            field.Reset();
+            DisableAllButton();
+            ActivateNeedButton();
+            DisplayField();
+            e.Handled = true;
+        }
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+            e.Handled = true;
+        }
+
+        private void r_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.R)
+                this.btnReset_Click(sender, e);
+        }        
+    }
+    internal class MyButton : Button
+    {
+        public int I { get; set; } = -1;
+        public int J { get; set; } = -1;
+    }
+}
---
+++ TagPlayWPF/TagPlayLogic.cs
@@ -0,0 +1,163 @@
+﻿namespace TagPlay
+{
+    //Класс для создание массива 4*4 с уникальными значениями от 0 до 15
+    class RandomField
+    {
+        const int rang = 4;
+        int[,] array = new int[rang, rang];
+        System.Random random = new System.Random();
+
+        //Свойства для хранение индексов нулевого числа
+        internal int indexI0 { get; private set; } = 0;
+        internal int indexJ0 { get; private set; } = 0;
+
+
+        internal RandomField()
+        {
+            InitArray();
+            FillFieldRandom();
+        }
+
+        internal void Reset()
+        {
+            InitArray();
+            FillFieldRandom();
+        }
+
+        internal bool Check()
+        {
+            return checkedField1() || checkedField2();
+        }
+
+        //Получение числа из массива по индксам
+        internal int getNumber(int i, int j)
+        {
+            return array[i, j];
+        }
+
+        //Функция для перестановки двух элементов по идексу i (в игровом поле)
+        internal void swapNumberByI(int newIndexI)
+        {
+            if (newIndexI < 0 || newIndexI > 3) return;
+
+            int temp = array[newIndexI, indexJ0];
+            array[newIndexI, indexJ0] = 0;
+            array[indexI0, indexJ0] = temp;
+            indexI0 = newIndexI;
+        }
+
+        //Функция для перестановки двух элементов по идексу j (в игровом поле)
+        internal void swapNumberByJ(int newIndexJ)
+        {
+            if (newIndexJ < 0 || newIndexJ > 3) return;
+
+            int temp = array[indexI0, newIndexJ];
+            array[indexI0, newIndexJ] = 0;
+            array[indexI0, indexJ0] = temp;
+            indexJ0 = newIndexJ;
+        }
+
+        //Функция отображающая игровое поле в консоли
+        //internal void displayField()
+        //{
+        //    for (int i = 0; i < rang; i++)
+        //    {
+        //        for (int j = 0; j < rang; ++j)
+        //        {
+        //            if (array[i, j] == 0)
+        //            {
+        //                System.Console.Write("O\t");
+        //                continue;
+        //            }
+        //            System.Console.Write(array[i, j] + "\t");
+        //        }
+        //        System.Console.Write("\n\n");
+        //    }
+        //}
+
+        //internal void test()
+        //{
+        //    for (int i = 0; i < rang; ++i)
+        //        for (int j = 0; j < rang; ++j)
+        //        {                    
+        //            array[i, j] = rang * i + j + 1;
+        //        }
+        //}
+        private bool checkedField1()
+        {
+            for (int i = 0; i < rang; ++i)
+                for (int j = 0; j < rang; ++j)
+                {
+                    if (array[i, j] != rang * i + j) return false;
+                }
+
+            return true;
+        }
+        private bool checkedField2()
+        {
+            for (int i = 0; i < rang; ++i)
+                for (int j = 0; j < rang; ++j)
+                {
+                    if (i == 3 && j == 3) break;
+                    if (array[i, j] != rang * i + j + 1) return false;
+                }
+            return true;
+        }        
+
+        //Инициализация массива -1
+        private void InitArray()
+        {
+            
+            for (int i = 0; i < rang; i++)
+                for (int j = 0; j < rang; j++)
+                    array[i, j] = -1;
+        }
+
+        //Заполнение массива уникальными числами
+        private void FillFieldRandom()
+        {
+            int temp = -1;
+
+            //Заполнение массива уникальными числами
+            for (int i = 0; i < rang; ++i)
+            {
+                for (int j = 0; j < rang; ++j)
+                {
+                    //Проверка сгенерированного числа на существование в массиве                                       
+                    while (true)
+                    {
+                    Begin: temp = random.Next(rang * rang);
+
+                        for (int k = 0; k < rang; ++k)
+                        {
+                            for (int l = 0; l < rang; ++l)
+                            {
+                                if (array[k, l] == temp)
+                                {
+                                    goto Begin;
+                                }
+                            }
+                        }
+                        break;
+                    }
+                    array[i, j] = temp;
+                }
+            }//Конец инициализации массива
+
+            //Поиск нулевого элемента
+            for (int i = 0; i < rang; ++i)
+            {
+                for (int j = 0; j < rang; ++j)
+                {
+                    if (array[i, j] == 0)
+                    {
+                        indexI0 = i;
+                        indexJ0 = j;
+                        goto Exit;
+                    }
+                }
+            }
+        Exit:;
+        }
+    }
+}
